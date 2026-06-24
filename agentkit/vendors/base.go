a69dd8126f97970231829baae4f2a3bade9c5bfe
seymourtang <tangxianmeng@live.com>
2026-06-24 21:34:10 +0800

    refactor(agentkit): share vendor interfaces across global and cn packages

--- agentkit/vendors/base.go
+++ agentkit/vendors/base.go
@@ -1,34 +1,22 @@
 package vendors
 
-type SampleRate int
+import core "github.com/AgoraIO/agora-agents-go/v2/agentkit/core"
+
+type SampleRate = core.SampleRate
 
 const (
-	SampleRate8kHz  SampleRate = 8000
-	SampleRate16kHz SampleRate = 16000
-	SampleRate22kHz SampleRate = 22050
-	SampleRate24kHz SampleRate = 24000
-	SampleRate44kHz SampleRate = 44100
-	SampleRate48kHz SampleRate = 48000
+	SampleRate8kHz  = core.SampleRate8kHz
+	SampleRate16kHz = core.SampleRate16kHz
+	SampleRate22kHz = core.SampleRate22kHz
+	SampleRate24kHz = core.SampleRate24kHz
+	SampleRate44kHz = core.SampleRate44kHz
+	SampleRate48kHz = core.SampleRate48kHz
 )
 
-type LLM interface {
-	ToConfig() map[string]interface{}
-}
-
-type TTS interface {
-	ToConfig() map[string]interface{}
-	GetSampleRate() *SampleRate
-}
-
-type STT interface {
-	ToConfig() map[string]interface{}
-}
-
-type MLLM interface {
-	ToConfig() map[string]interface{}
-}
-
-type Avatar interface {
-	ToConfig() map[string]interface{}
-	RequiredSampleRate() SampleRate
-}
+type (
+	LLM    = core.LLMVendor
+	TTS    = core.TTSVendor
+	STT    = core.STTVendor
+	MLLM   = core.MLLMVendor
+	Avatar = core.AvatarVendorConfig
+)
